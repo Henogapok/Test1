a95b08295b035a14a10ec51bdce0d6b6d3db91af
Henogapok <aprelevvaleriy@gmail.com>
2024-04-15 21:34:49 +0600

    Edited main.py

--- main.py
+++ main.py
@@ -7,6 +7,5 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('Hello, World!')
-    print(1, 2, 3, 4, 5)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
